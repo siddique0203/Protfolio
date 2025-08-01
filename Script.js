73213d6b9bb24f29d0047ea3ab3fd8728223c1e2
Abu Bakar Siddique <siddique0203@gmail.com>
2025-08-01 23:00:27 +0600

    first commit

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.getElementById("appointment").addEventListener("submit", function (e) {
+    e.preventDefault();
+    let randomInt = Math.floor(Math.random() * 9000) + 1000;
+    alert(`Taken Appointment, Token No: ${randomInt}`);
+    window.location.href = "Two.html"
+    
+});
